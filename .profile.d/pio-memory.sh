f78c0fd3b0cce183569429044362ceafc7092ebe
Mars Hall <mars@users.noreply.github.com>
2017-06-19 14:41:24 -0700

    Decrease Spark memory to avoid R14 soft-limit errors.

--- .profile.d/pio-memory.sh
+++ .profile.d/pio-memory.sh
@@ -16,19 +16,19 @@ function already_has_memory_opts() {
 limit=$(ulimit -u)
 case $limit in
 256)   # 512MB (Free, Hobby, Standard-1X)
-  default_spark_opts="--driver-memory 512m"
+  default_spark_opts="--driver-memory 384m"
   ;;
 512)   # 1024MB (Standard-2X, Private-S)
-  default_spark_opts="--driver-memory 1024m"
+  default_spark_opts="--driver-memory 768m"
   ;;
 16384) # 2560MB (Performance-M, Private-M)
-  default_spark_opts="--driver-memory 2560m"
+  default_spark_opts="--driver-memory 2024m"
   ;;
 32768) # 14GB (Performance-L, Private-L)
-  default_spark_opts="--driver-memory 14g"
+  default_spark_opts="--driver-memory 13g"
   ;;
 *)
-  default_spark_opts="--driver-memory 512m"
+  default_spark_opts="--driver-memory 384m"
   ;;
 esac
 
